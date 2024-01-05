2eddcf8544d3a0d2862f82ba496715729cc3db97
Brandon Hoffman <branhoff2@gmail.com>
2024-01-04 22:39:48 -0800

    importing the new form_routes module

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-from app.routes import jira_routes, chat_gpt_routes
+from app.routes import jira_routes, chat_gpt_routes, form_routes
 
 app.register_blueprint(jira_routes.jira_bp)
 app.register_blueprint(chat_gpt_routes.chatgpt_bp)
